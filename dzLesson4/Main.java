bd0ae99bb2f79a3a1a5a10c7a2c134c15e818c1a
Vitjat <71880844+Vitjat@users.noreply.github.com>
2021-01-04 02:02:58 +0700

    Update Main.java

--- dzLesson4/Main.java
+++ dzLesson4/Main.java
@@ -1,4 +1,44 @@
 package dzLesson4;
 
-public class Main {
+// второе задание в процессе
+
+public class MainOne {
+    static volatile char c = 'A';
+    static Object mon = new Object();
+
+    public static void main(String[] args) {
+        System.out.println("Flow");
+        new Thread(new WaitNotifyAll('A', 'B')).start();
+        new Thread(new WaitNotifyAll('B', 'C')).start();
+        new Thread(new WaitNotifyAll('C', 'A')).start();
+    }
+
+    static class WaitNotifyAll implements Runnable {
+        private char currentLetter;
+        private char nextLetter;
+
+        public WaitNotifyAll(char currentLetter, char nextLetter) {
+            this.currentLetter = currentLetter;
+            this.nextLetter = nextLetter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 5; i++) {
+                synchronized (mon) {
+                    try {
+                        while (c != currentLetter)
+                            mon.wait();
+                        System.out.print(currentLetter);
+                        c = nextLetter;
+                        mon.notifyAll();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
 }
+
